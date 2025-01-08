b8ecc2f8315b19268e08398d1594a9bf0699c8e8
Safarboy ibn Anvar al-Xorazmiy <safarboyalkhwarizmi@gmail.com>
2025-01-09 02:43:48 +0500

    Navbar fully responsive compleated

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 
 const App = () => {
-
   return (
-    <div className="bg-gradient-main min-h-screen px-4 flex flex-col items-center text-white font-medium">
-      <div className="w-full z-10 min-h-screen flex flex-col items-center text-white">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+    </Routes>
   );
 };
 
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import logo from '../images/logo.svg';
+import menuIcon from '../images/menu-icon.svg';
+import backIcon from '../images/back-icon.svg';
 
 const Dashboard: React.FC = () => {
+  const [menuOpened, setMenuOpened] = React.useState(false); 
+
   return (
     <div>
+      <div className={`clear ${menuOpened ? 'open' : ''}`}>
+        <img src={backIcon} className="back-icon" alt="" onClick={() => {
+          setMenuOpened(!menuOpened);
+        }} />
+      </div>
+
       <header>
         <nav>
           <a href="/">
-            <img src="../images/logo.svg" alt="" />
+            <img src={logo} alt="" />
           </a>
 
-          <ul>
-            <li>Packages</li>
-            <li>Contact</li>
-            <li>Certificate</li>
+          <ul className={menuOpened ? 'open' : ''}>
+            <li><a href="">Packages</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="/certificate">Certificate</a></li>
           </ul>
 
-          <button className="btn btn-primary">Start Your Journey</button>
-        </nav>
+          <div className="buttons">
+            <button className={`btn btn-primary  ${menuOpened ? 'open' : ''}`}>
+              Start Your Journey
+              <span className="btn-primary__inner">
+                <span className="btn-primary__blobs">
+                  <span className="btn-primary__blob"></span>
+                  <span className="btn-primary__blob"></span>
+                  <span className="btn-primary__blob"></span>
+                  <span className="btn-primary__blob"></span>
+                </span>
+              </span>
+            </button>
+          </div>
 
-        
+          <div 
+            className={`menu-icon ${menuOpened ? 'open' : ''}`}
+            onClick={() => setMenuOpened(!menuOpened)}>
+            <img src={menuIcon} alt="" />
+          </div>
+        </nav>
       </header>
     </div>
   );
