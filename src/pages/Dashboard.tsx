734475a28c4a2266317a811039b37d837ff1bf40
Safarboy ibn Anvar al-Xorazmiy <safarboyalkhwarizmi@gmail.com>
2025-01-09 03:18:13 +0500

    Header main and Service section compleated no responsive

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import logo from '../images/logo.svg';
 import menuIcon from '../images/menu-icon.svg';
 import backIcon from '../images/back-icon.svg';
+import playIcon from '../images/play-icon.svg';
+import vehicle1 from '../images/vehicle-1.png';
+import vehicle2 from '../images/vehicle-2.png';
+import vehicle3 from '../images/vehicle-3.png';
+import vehicle4 from '../images/vehicle-4.png';
 
 const Dashboard: React.FC = () => {
   const [menuOpened, setMenuOpened] = React.useState(false); 
@@ -46,7 +51,47 @@ const Dashboard: React.FC = () => {
             <img src={menuIcon} alt="" />
           </div>
         </nav>
+
+        <main>
+          <h3>Reliable Transportation. </h3>
+          <h1>Based in Abu Dhabi. </h1>
+          <h2>Providing service all over UAE. </h2>
+
+          <button className="btn btn-outline-primary">
+            <img src={playIcon} alt="" />
+            Explore more
+          </button>
+        </main>
       </header>
+      
+      <section id="service">
+        <h2>
+          Reliable passengers transportation via rented <strong>buses</strong> for a group rides and <strong>light vehicles</strong> for your comfort trip - tailored to your needs.
+        </h2>
+
+        <h1 className="title">
+          SERVICE
+        </h1>
+
+        <div className="vehicles">
+          <div className="vehicle">
+            <img src={vehicle1} alt="" />
+          </div>
+          
+          <div className="vehicle">
+            <img src={vehicle2} alt="" />
+          </div>
+          
+          <div className="vehicle">
+            <img src={vehicle3} alt="" />
+          </div>
+
+          <div className="vehicle">
+            <img src={vehicle4} alt="" />
+          </div>
+        </div>
+
+      </section>
     </div>
   );
 };
